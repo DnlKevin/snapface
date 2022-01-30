66f96fe121bda49766fb14d611cd679c3ea0ca52
user.email <Kevin.danielm06@gmail.com>
2022-01-30 11:57:09 +0100

    Réagir aux évenements

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -11,6 +11,7 @@ export class FaceSnapComponent implements OnInit {
   createdDate!: Date;
   snaps!: number;
   imageUrl!: string;
+  buttonText!: string;
 
   ngOnInit(): void {
     this.title = 'Archibald';
@@ -19,5 +20,16 @@ export class FaceSnapComponent implements OnInit {
     this.snaps = 6;
     this.imageUrl =
       'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+    this.buttonText = 'Oh Snap !';
+  }
+
+  onSnap(): void {
+    if (this.buttonText === 'Oh Snap !') {
+      this.snaps++;
+      this.buttonText = 'Oops, unSnap !';
+    } else {
+      this.snaps--;
+      this.buttonText = 'Oh Snap !';
+    }
   }
 }
